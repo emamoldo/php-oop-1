41655c9d2fce320f9fc6df3354608f903ce56a11
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-18 17:24:05 +0200

    Add the Html Structure and the General.php file with the title, the language and the vote

---
+++ Models/General.php
@@ -0,0 +1,14 @@
+<?php
+// This is the class
+class General
+{
+    public $title;
+    public $language;
+    public $vote;
+}
+
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . "/Models/General.php";
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oop</title>
+</head>
+
+<body>
+    <h1>Oop</h1>
+</body>
+
+</html>
