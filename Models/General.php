ab8b4492bedeb340e304a74321f856554bd499d1
emamoldo <moldovanemanuel92.em@gmail.com>
2024-04-18 17:41:57 +0200

    Print the value of the objects

--- Models/General.php
+++ Models/General.php
@@ -5,6 +5,23 @@ class General
     public $title;
     public $language;
     public $vote;
+
+
+    public function __construct($title, $language, $vote)
+    {
+        $this->title = $title;
+        $this->language = $language;
+        $this->vote = $vote;
+    }
+
+    // public function getTitle()
+    // {
+    //     echo $this->title;
+    // }
+
+    // $italy->title = "Italia";
+    // $italy->language = "Italiano";
+    // $italy->vote = "9";
 }
 
 
--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 require_once __DIR__ . "/Models/General.php";
 
+
+$usa = new General("Gli Usa", "English", 8);
+var_dump($usa);
+
+
+$italy = new General("L'Italia", "Italiano", 9);
+var_dump($italy);
+
+var_dump($usa->title, $usa->vote);
+var_dump($italy->title, $italy->vote);
+
+
 ?>
 
 
